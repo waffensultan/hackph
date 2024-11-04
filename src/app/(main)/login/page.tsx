b2131396ed657efe1c89791e66c526822ed77049
Waffen Sultan <waffen.sultan@gmail.com>
2024-11-04 20:34:52 +0800

    feat: added responsive success and error states

--- src/app/(main)/login/page.tsx
+++ src/app/(main)/login/page.tsx
@@ -9,25 +9,54 @@ import { WandSparkles as WandIcon, Quote as QuoteIcon } from "lucide-react";
 import ShineBorder from "@/packages/ui/components/ui/shine-border";
 import { Input } from "@/packages/ui/components/ui/input";
 import { Label } from "@/packages/ui/components/ui/label";
-import { Button } from "@/packages/ui/components/ui/button";
+
+type AuthStatus = "error" | "success";
 
 export default function Page() {
     const [email, setEmail] = useState("");
     const [loading, setLoading] = useState(false);
-
-    const emailIsValid = email.length > 4;
+    const [status, setStatus] = useState<undefined | AuthStatus>(undefined);
 
     const handleSubmit = async () => {
         setLoading(true);
 
         try {
             await signInWithEmail(email!);
+            setStatus("success");
         } catch (error) {
-            console.log("Error authenticating:", error);
+            setStatus("error");
+
+            const timeout = setTimeout(() => {
+                setStatus(undefined);
+            }, 2000);
+            return () => clearTimeout(timeout);
         } finally {
             setLoading(false);
         }
     };
+
+    const styling = {
+        button:
+            status === "success"
+                ? "bg-green-500 pointer-events-none opacity-80"
+                : status === "error"
+                ? "bg-red-500"
+                : "bg-primary",
+        text:
+            status === "success"
+                ? "Success! Please check your email."
+                : status === "error"
+                ? "Error! Try again."
+                : "Send Magic Link",
+        border:
+            status === "success"
+                ? "border-green-500 shadow-green-700"
+                : status === "error"
+                ? "border-red-500 shadow-red-700"
+                : "border-primary shadow-primary",
+    };
+
+    const emailIsValid = email.length > 4;
 
     return (
         <main className="flex flex-col gap-5 justify-center items-center pt-28">
@@ -35,9 +64,9 @@ export default function Page() {
                 🎉 We're glad to be having you!
             </h1>
             <ShineBorder
-                className={`relative flex flex-col gap-3 transition duration-150 ${
+                className={`relative flex flex-col gap-3 transition duration-150 border ${
                     emailIsValid ? "shadow-xl" : "shadow-md"
-                } shadow-primary border border-primary rounded-[var(--radius)] py-14 px-8 md:p-14`}
+                } ${styling.border} rounded-[var(--radius)] py-14 px-8 md:p-14`}
                 color={["#A07CFE", "#FE8FB5", "#FFBE7B"]}
             >
                 <section className="flex flex-col gap-1 w-full">
@@ -55,15 +84,14 @@ export default function Page() {
                         name="email"
                         placeholder="Enter your email here..."
                     />
-                    <Button
+                    <button
                         onClick={() => handleSubmit()}
                         disabled={!emailIsValid || loading}
-                        variant={"default"}
-                        className="mt-2"
+                        className={`${styling.button} mt-2 text-primary-foreground flex items-center justify-center gap-3 py-2 rounded-[var(--radius)] transition duration-150 disabled:pointer-events-none disabled:opacity-50`}
                     >
-                        <WandIcon />
-                        <span className="text-lg">Send Magic Link</span>
-                    </Button>
+                        {!status && <WandIcon />}
+                        <span className="text-lg">{styling.text}</span>
+                    </button>
                 </section>
                 <footer className="max-w-xs mt-5 p-4 rounded-[var(--radius)] bg-muted/50 text-muted-foreground text-sm flex flex-row gap-3">
                     <div className="flex">
